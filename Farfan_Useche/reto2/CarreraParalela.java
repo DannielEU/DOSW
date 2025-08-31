d2ed9496659a106249d53512a76c9900e964aa93
Jared <Jared.farfan-g@mail.escuelaing.edu.co>
2025-08-31 12:21:24 -0500

    gran meta carril numero 2

--- Farfan_Useche/reto2/CarreraParalela.java
+++ Farfan_Useche/reto2/CarreraParalela.java
@@ -43,21 +43,37 @@ public class CarreraParalela {
     private boolean listaPar( ArrayList<Integer> lista){
         return (lista.size() % 2 == 0) ? true : false;
     }
-    public List<String> informacionDeLista(ArrayList<Integer> lista) {
-        List<String> info = new ArrayList<>();
+    public String[][] informacionDeLista(ArrayList<Integer> lista1, ArrayList<Integer> lista2){
+        String[][] informacion = new String[7][2];
+        //mayor
+        int mayor1 = mayorNumero(lista1);
+        int mayor2 = mayorNumero(lista2);
+        informacion[0][0] = ""+(mayor1);
+        informacion[1][0] = ""+(mayor2);
+        //menor
+        informacion[0][1] = ""+(menorNumero(lista1)[0]);
+        informacion[1][1] = ""+(menorNumero(lista2)[0]);
+        //cantidad
+        informacion[0][2] = ""+(menorNumero(lista1)[1]);
+        informacion[1][2] = ""+(menorNumero(lista2)[1]);
+        //multiplo
+        informacion[0][3] = ""+(multiplode2(mayor1));
+        informacion[1][3] = ""+(multiplode2(mayor2));
+        //divisor
+        informacion[0][4] = ""+(divisorDeDos(mayor1));
+        informacion[1][4] = ""+(divisorDeDos(mayor2));
+        //par
+        informacion[0][5] = ""+(listaPar(lista1));
+        informacion[1][5] = ""+(listaPar(lista2));
+        //impar
+        informacion[0][6] = ""+(cantidadInpar(lista1));
+        informacion[1][6] = ""+(cantidadInpar(lista2));
 
-        int mayor = mayorNumero(lista);
-        int menor = menorNumero(lista);
-        info.add("Lista: " + lista);
-        info.add("Menor: " + menor);
-        info.add("Mayor: " + mayor);
-        info.add("¿El mayor es múltiplo de 2? " + multiplode2(mayor));
-        info.add("¿El mayor es divisor de 2? " + divisorDeDos(mayor));
-
-        return info;
+        
+        return informacion;
     }
 
-    public boolean cantidadInpar(ArrayList<Integer> lista){
+    public boolean cantidadImpar(ArrayList<Integer> lista){
         return (lista.size() % 2 != 0)?true:false;
     }
 }
