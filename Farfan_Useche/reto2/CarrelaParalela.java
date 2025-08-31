6771e1092979a2e4e95aa02327fde344b6f882bc
Daniel Eduardo Useche <du.important@gmail.com>
2025-08-31 09:28:44 -0500

    feature/listaMayor

--- Farfan_Useche/reto2/CarrelaParalela.java
+++ Farfan_Useche/reto2/CarrelaParalela.java
@@ -2,8 +2,12 @@ package Farfan_Useche.reto1;
 
 import java.util.*;
 
-public class Main {
+public class CarrelaParalela {
     public static void main(String[] args) {
-
+        System.out.println("Hola");
+    }
+    public int MayorNumero( ArrayList lista){
+        int numero = lista.stream().max((a,b) -> a.compareTo(b)).get());
+        return numero;
     }
 }
