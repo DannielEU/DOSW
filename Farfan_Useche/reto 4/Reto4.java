2899c48fc3b4833d943372da9b9883be62b3cccb
Jared <Jared.farfan-g@mail.escuelaing.edu.co>
2025-08-31 21:19:44 -0500

    Ordenar por clave

--- Farfan_Useche/reto 4/Reto4.java
+++ Farfan_Useche/reto 4/Reto4.java
@@ -4,14 +4,18 @@ import java.util.Hashtable;
 
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Reto4{
     private HashMap<String, Integer> map = new HashMap<>();
     private Hashtable<String, Integer> tabla = new Hashtable<>();
     private HashMap<String, Integer> combinado = new HashMap<>();
 
-    public static void main(String[] args) {
-        System.out.println("Hola");
+    public static void main(HashMap<String, Integer> map, Hashtable<String, Integer> tabla) {
+        this.map = map;
+        this.tabla =tabla;
     }
 
     public void almacernarHashtable(String[][] claveValor){
@@ -19,12 +23,6 @@ public class Reto4{
             if (!tabla.containsKey(info[0])) {
                 tabla.put(info[0], Integer.parseInt(info[1]));
             }
-        }
-    }
-    
-    public void reto4(String[][] args){
-        for (String[] s : args) {
-            this.map.put(s[0], Integer.parseInt(s[1]));
         }
     }
 
@@ -36,12 +34,26 @@ public class Reto4{
             combinado.put(clave, tabla.get(clave));
         }
     }
+
     public HashMap<String, Integer> clavesMayusculas(HashMap<String, Integer> original) {
         HashMap<String, Integer> nuevo = new HashMap<>();
         for (String clave : original.keySet()) {
             nuevo.put(clave.toUpperCase(), original.get(clave));
         }
         return nuevo;
+    }
+
+    public Map<String, Integer> ordenarClaves(HashMap<String, Integer> original){
+        Map<String, Integer> mapaOrdenado = original.entrySet()
+            .stream()
+            .sorted(Map.Entry.comparingByKey()) // compara por clave
+            .collect(Collectors.toMap(
+                Map.Entry::getKey,
+                Map.Entry::getValue,
+                (e1, e2) -> e1,
+                LinkedHashMap::new
+            ));
+        return mapaOrdenado;
     }
 
 }
