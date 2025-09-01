35b5d297bb50af665539c44957aa1e3fd5f83df5
Jared <Jared.farfan-g@mail.escuelaing.edu.co>
2025-08-31 20:47:53 -0500

    combinadondo maps

--- Farfan_Useche/reto 4/Reto4.java
+++ Farfan_Useche/reto 4/Reto4.java
@@ -7,20 +7,19 @@ import java.util.HashMap;
 
 public class Reto4{
     private HashMap<String, Integer> map = new HashMap<>();
-
-    public Hashtable<String, Integer> tabla = new Hashtable<>();
+    private Hashtable<String, Integer> tabla = new Hashtable<>();
+    private HashMap<String, Integer> combinado = new HashMap<>();
 
     public static void main(String[] args) {
         System.out.println("Hola");
     }
 
-    public Hashtable<String, Integer> almacernarHashtable(String[][] claveValor){
+    public void almacernarHashtable(String[][] claveValor){
         for (String[] info : claveValor) {
             if (!tabla.containsKey(info[0])) {
                 tabla.put(info[0], Integer.parseInt(info[1]));
             }
         }
-        return tabla;
     }
     
     public void reto4(String[][] args){
@@ -28,4 +27,16 @@ public class Reto4{
             this.map.put(s[0], Integer.parseInt(s[1]));
         }
     }
+
+    public void combinado(){
+        for (Map.Entry<String, Integer> en : this.tabla.entrySet()) {
+            combinado.put(en.getKey(),en.getValue());
+        }
+        for (Map.Entry<String, Integer> en : this.map.entrySet()) {
+            if (!map.containsKey(en.getKey()){
+                tabla.put(en.getKey(), en.getValue());
+            }
+        }
+    }
+
 }
