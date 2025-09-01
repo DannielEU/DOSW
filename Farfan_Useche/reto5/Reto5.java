e5818a37dad97be334718922c3af543e1a4f9f99
Daniel Eduardo Useche <du.important@gmail.com>
2025-08-31 22:22:08 -0500

    Funcion organizar quitar

--- Farfan_Useche/reto5/Reto5.java
+++ Farfan_Useche/reto5/Reto5.java
@@ -6,6 +6,14 @@ public class Reto5 {
        System.out.println("Hello World");
     }
 
-    
+    public TreeSet<Integer> procesarNumeros(int[] numerosEntrada) {
+        TreeSet<Integer> numeros = new TreeSet<>();
 
+        for (int num : numerosEntrada) {
+            numeros.add(num);
+        }
+        numeros.removeIf(n -> n % 5 == 0);
+
+        return numeros;
+    }
 }
