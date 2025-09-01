064d083a723a0de2ab1cb6c97ca44a2a42c8bbc6
Daniel Eduardo Useche <du.important@gmail.com>
2025-08-31 20:30:03 -0500

    funcion lectura

--- Farfan_Useche/reto 4/Reto4.java
+++ Farfan_Useche/reto 4/Reto4.java
@@ -1,12 +1,18 @@
 package Farfan_Useche.reto4;
 
 
+import java.util.HashMap;
 
 public class Reto4{
+    private HashMap<String, Integer> map = new HashMap<>();
 
     public static void main(String[] args) {
         System.out.println("Hola");
     }
     
-    public Hashtable 
+    public void reto4(String[][] args){
+        for (String[] s : args) {
+            this.map.put(s[0], Integer.parseInt(s[1]));
+        }
+    }
 }
