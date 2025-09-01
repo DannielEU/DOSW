47b73c16cebae5d525fe0f85d2c3dabe1a48ffa5
Jared <Jared.farfan-g@mail.escuelaing.edu.co>
2025-08-31 22:21:24 -0500

    hashSet

--- Farfan_Useche/reto5/Reto5.java
+++ Farfan_Useche/reto5/Reto5.java
@@ -1,11 +1,25 @@
 package Farfan_Useche.reto5;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Reto5 {
 
+    private HashSet<Integer> hash;
     public static void main(String[] args) {
        System.out.println("Hello World");
     }
 
-    
+    public static Set<Integer> equipoHashSet(Integer[] numeros) {
+        HashSet<Integer> equipo = new HashSet<>();
+        for (int i : numeros) {
+            equipo.add(i);
+        }
+        Set<Integer> filtrado = equipo.stream()
+                .filter(n -> n % 3 != 0)
+                .collect(Collectors.toSet());
+        return filtrado;
+    }
 
 }
