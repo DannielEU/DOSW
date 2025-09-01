8a647d467c8e5077872e9963e58a69b1e981c75b
Jared <Jared.farfan-g@mail.escuelaing.edu.co>
2025-08-31 20:25:47 -0500

    funcion para Hashtable

--- Farfan_Useche/reto 4/Reto4.java
+++ Farfan_Useche/reto 4/Reto4.java
@@ -1,12 +1,24 @@
 package Farfan_Useche.reto4;
 
+import java.util.Hashtable;
+
 
 
 public class Reto4{
 
+    public Hashtable<String, Integer> tabla = new Hashtable<>();
+
     public static void main(String[] args) {
         System.out.println("Hola");
     }
-    
-    public Hashtable 
+
+    public Hashtable<String, Integer> almacernarHashtable(String[][] claveValor){
+        for (String[] info : claveValor) {
+            if (!tabla.containsKey(info[0])) {
+                tabla.put(info[0], Integer.parseInt(info[1]));
+            }
+        }
+        return tabla;
+    }
+
 }
