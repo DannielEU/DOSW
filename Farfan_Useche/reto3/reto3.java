eac736d214776080813074b67d3a18268a172552
Jared <Jared.farfan-g@mail.escuelaing.edu.co>
2025-08-31 19:41:20 -0500

    funcion combinada

--- Farfan_Useche/reto3/reto3.java
+++ Farfan_Useche/reto3/reto3.java
@@ -1,22 +1,24 @@
 package Farfan_Useche.reto3;
 
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Reto3{
 
     public static void main(String[] args) {
         System.out.println("Hola");
+
     }
 
     public static String repetirMensaje(String str) {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < 3; i++) {
-        sb.append(str);
-        if (i < 2) {
-            sb.append(" ");
-        }
-    }
-    return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        // Uso de stream para repetir 3 veces
+        String repetido = IntStream.range(0, 3)
+                .mapToObj(i -> str)
+                .collect(Collectors.joining(" "));
+
+        sb.append(repetido);
+        return sb.toString();
     }
 
     public static String buffer(String texto) {
